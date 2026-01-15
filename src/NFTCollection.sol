33991259936e1d1a8fb98e4547f3c6a83c4976c8
Aiman Khan <aiman@Aimans-MacBook-Air.local>
2026-01-16 03:37:29 +0530

    Add feature to update BaseURI, pause/resume minting and withdraw deposits

--- src/NFTCollection.sol
+++ src/NFTCollection.sol
@@ -10,19 +10,25 @@ contract NFTCollection is ERC721, Ownable, INFTCollection, NFTCollectionEvents {
     uint256 private nextTokenId;
     uint256 public maxSupply;
     string private baseTokenURI;
+    bool public mintingPaused;
+    uint256 public mintPrice;
 
     constructor(
         string memory name,
         string memory symbol,
         uint256 _maxSupply,
-        string memory _baseTokenURI
+        string memory _baseTokenURI,
+        uint256 _mintPrice
     ) ERC721(name, symbol) Ownable(msg.sender) {
         maxSupply = _maxSupply;
         baseTokenURI = _baseTokenURI;
+        mintPrice = _mintPrice;
     }
 
-    function mint(address _to) external override returns (uint256) {
+    function mint(address _to) external payable override returns (uint256) {
         require(nextTokenId < maxSupply, "Cannot mint more tokens");
+        require(msg.value >= mintPrice, "Insufficient payment");
+        require(!mintingPaused, "Minting is paused");
         uint256 tempNextTokenId = nextTokenId;
         nextTokenId++;
         _safeMint(_to, tempNextTokenId);
@@ -38,5 +44,36 @@ contract NFTCollection is ERC721, Ownable, INFTCollection, NFTCollectionEvents {
 
     function baseURI() external view override returns (string memory) {
         return baseTokenURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string memory _newBaseTokenURI) external onlyOwner {
+        string memory tempBaseTokenURI = baseTokenURI;
+
+        baseTokenURI = _newBaseTokenURI;
+        emit BaseURIUpdated(tempBaseTokenURI, baseTokenURI);
+    }
+
+    function pauseMinting() external onlyOwner {
+        require(!mintingPaused, "Minting is already paused");
+
+        mintingPaused = true;
+        emit MintingPaused(true);
+    }
+
+    function resumeMinting() external onlyOwner {
+        require(mintingPaused, "Minting is not paused");
+
+        mintingPaused = false;
+        emit MintingPaused(false);
+    }
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Insufficient balance");
+        (bool success, ) = payable(owner()).call{value: balance}("");
+        require(success, "Withdraw failed");
     }
 }
--- src/events/NFTCollectionEvents.sol
+++ src/events/NFTCollectionEvents.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.8.20;
 abstract contract NFTCollectionEvents {
     event NFTMinted(address indexed to, uint256 indexed tokenId);
     event BaseURIUpdated(string oldURI, string newURI);
+    event MintingPaused(bool isPaused);
 }
